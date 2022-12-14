9b8fc5bbb53724abecce4e3664559b00a7fdff41
Zethi <diego1900196@gmail.com>
2022-12-14 07:43:18 -0300

    optimized setImage algorithm (this version radically reduces ram usage, but cpu usage increases)

--- src/main/java/com/github/zethi/Main.java
+++ src/main/java/com/github/zethi/Main.java
@@ -1,27 +1,22 @@
 package com.github.zethi;
 
-
-import java.awt.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.stage.Window;
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
     private Dimensions dimensions;
     private final Screen screen = Screen.getPrimary();
     private final Stage program = new Stage();
     private String actualScreen;
-
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/com/github/zethi/VirtualScreen.java
+++ src/main/java/com/github/zethi/VirtualScreen.java
@@ -1,9 +1,8 @@
 package com.github.zethi;
 
-import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.stage.Screen;
 
 import java.awt.*;
@@ -16,16 +15,14 @@ public class VirtualScreen {
     private final Robot robot;
     private String actualScreen;
 
-
     public VirtualScreen(Dimensions dimensions, Robot robot) {
         this.robot = robot;
         this.dimensions = dimensions;
         this.actualScreen = MouseInfo.getPointerInfo().getDevice().getIDstring();
     }
 
-
     public Dimensions updatePropertyToActualScreen() {
-        if(actualScreen.equals(MouseInfo.getPointerInfo().getDevice().getIDstring())) return this.dimensions;
+        if (actualScreen.equals(MouseInfo.getPointerInfo().getDevice().getIDstring())) return this.dimensions;
         actualScreen = MouseInfo.getPointerInfo().getDevice().getIDstring();
         Screen currentScreen = Screen.getScreensForRectangle(new Rectangle2D(
                 MouseInfo.getPointerInfo().getLocation().getX(),
@@ -41,8 +38,16 @@ public class VirtualScreen {
 
     public void setImage(Dimensions dimensions, GraphicsContext graphicContext) {
         BufferedImage screenImage = robot.createScreenCapture(new Rectangle((int) dimensions.getScreenX(), (int) dimensions.getScreenY(),
-                                                                            (int) dimensions.getScreenWidth(), (int) dimensions.getScreenHeight()));
-        Image image = SwingFXUtils.toFXImage(screenImage, null);
-        graphicContext.drawImage(image, 0, 0);
+                (int) dimensions.getScreenWidth(), (int) dimensions.getScreenHeight()));
+
+        WritableImage fxImage = new WritableImage(screenImage.getWidth(), screenImage.getHeight());
+
+        for (int x = 0; x < screenImage.getWidth(); x++) {
+            for (int y = 0; y < screenImage.getHeight(); y++) {
+                fxImage.getPixelWriter().setArgb(x, y, screenImage.getRGB(x, y));
+            }
+        }
+
+        graphicContext.drawImage(fxImage, 0, 0);
     }
 }
